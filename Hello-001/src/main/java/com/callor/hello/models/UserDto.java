28f82614ecc261678fc012105af28a6883d913ea
n96js <n96js@naver.com>
2024-03-26 15:34:37 +0900

    hello

---
+++ Hello-001/src/main/java/com/callor/hello/models/UserDto.java
@@ -0,0 +1,70 @@
+package com.callor.hello.models;
+
+public class UserDto {
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	private String tel;
+	
+	
+	
+	// alt + shift + s super class
+	public UserDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	// alt + shift + s using field
+	public UserDto(String username, String password, String name, String email, String tel) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+		this.tel = tel;
+	}
+	
+	
+	// alt + shift + s getter and setter  
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	// alt + shift + s tostring
+	@Override
+	public String toString() {
+		return "UserDto [username=" + username + ", password=" + password + ", name=" + name + ", email=" + email
+				+ ", tel=" + tel + "]";
+	}
+	
+	
+}
--- Hello-001/src/main/webapp/WEB-INF/views/home.jsp
+++ Hello-001/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,12 +1,42 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+	
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:set var="rootPath" value="${pageContext.request.contextPath}" />
+<!-- rootPath 라는 변수를 선언해서 value의 값을 담겠다 -->
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8" />
-    <title>Insert title here</title>
-  </head>
-  <body>
-    <h1>Hello Welcome Nice to meet you</h1>
-    <h2>The time on the Today is ${serverTime}</h2>
-  </body>
+<head>
+<meta charset="UTF-8" />
+<title>Insert title here</title>
+<!-- ${pageContext.request.contextPath } 전역변수 -->
+<!-- ?001 ?002
+	링크는 ?뒤에를 무시하지만
+	tomcat은 무시하지않는데,
+	?002라는 파일이 없기때문에 무시
+	css파일을 수정하고 변경되었다는걸 알려주기위한 하나의 속임수
+-->
+<!-- <link href="${pageContext.request.contextPath }/static/css/main.css?002" rel="stylesheet"> -->
+<link href="${rootPath}/static/css/main.css?001" rel="stylesheet">
+</head>
+<body>
+	<header class="main">
+		<h1>반갑습니다</h1>
+	</header>
+	<nav class="main">
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/notice">공지사항</a></li>
+			<li><a href="bbs">자유게시판</a></li>
+			<li><a href="${rootPath}/user/login">로그인</a></li>
+			<li><a href="${rootPath}/user/join">회원가입</a></li>
+				
+			
+				
+		</ul>
+	</nav>
+	
+</body>
 </html>
