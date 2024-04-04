514ca0cf024d0e6e7ceef8cbc5c7a21dd34d9d58
n96js <n96js@naver.com>
2024-04-04 15:44:10 +0900

    ioListLogin

--- SpMVC-iolist-01/src/main/java/com/callor/iolist/controller/IolistController.java
+++ SpMVC-iolist-01/src/main/java/com/callor/iolist/controller/IolistController.java
@@ -9,13 +9,18 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.callor.iolist.models.IoListVO;
+import com.callor.iolist.models.UserVO;
 import com.callor.iolist.persistance.IoListDao;
+import com.callor.iolist.utils.NamesValue;
 
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
@@ -41,7 +46,22 @@ public class IolistController {
 	}
 	
 	@RequestMapping(value="/insert", method=RequestMethod.GET)
-	public String insert(Model model) {
+	public String insert(Model model,HttpSession httpSession) {
+		
+		/*
+		 * HttpSession 에 저장된 session 정보는 type 이 Object 이다
+		 * 그래서 실제 상황에서는 필요한 객체 type 으로 Casting(형변환)을 해야한다.
+		 * 
+		 * float num1 = 10.0f;
+		 * int num2 = (int) num1;
+		 */
+		UserVO userVO = (UserVO) httpSession.getAttribute(NamesValue.SESSION.USER);
+		
+		// 로그인이 됐는지 확인하는 코드, 되어있지 않으면 login 페이지로 이동
+		if(userVO == null) {
+			return "redirect:/user/login?error=needs";
+		}
+		
 		// 날짜와 관련된 java 1.8 이전버전의 클래스
 				Date today = new Date();
 				// 몇일전 몇일후 계산과 관련된 클래스, 문자열로 만들땐 format 같은것이 필요없음
@@ -81,11 +101,22 @@ public class IolistController {
 
 	}
 	
-	@RequestMapping(value="/insert", method=RequestMethod.POST)
-	public String insert(IoListVO iolistVO, Model model) {
+	/*
+	 * POST /insert 와 POST /update/seq 로 요청이 들어오면
+	 * 모두 처리하는 method
+	 */
+	
+	@RequestMapping(value={"/insert","/update/{seq}"}, method=RequestMethod.POST)
+	public String insertOrUpdate(@PathVariable(name = "seq", required = false, value="")
+									// seq 값이 필수는 아님, 값이 없으면 ""  
+			String seq,IoListVO iolistVO, Model model) {
+		
+		if(seq != null) {
+			iolistVO.setIo_seq(Long.valueOf(seq));
+		}
 		
 		log.debug(iolistVO.toString());
-		int result = iolistDao.insert(iolistVO);
+		int result = iolistDao.insertOrUpdate(iolistVO);
 		if(result > 0) {
 			return "redirect:/iolist/";
 			
@@ -94,6 +125,38 @@ public class IolistController {
 			return "layout";
 		}
 		
+	}
+	
+	@RequestMapping(value="/detail/{seq}", method=RequestMethod.GET)
+	public String detail(@PathVariable("seq") String seq, Model model) {
+		
+		Long io_seq = Long.valueOf(seq);
+		IoListVO vo = iolistDao.findBySeq(io_seq);
+		
+		model.addAttribute("IO", vo);
+		
+		model.addAttribute("BODY", "IOLIST_DETAIL");
+		return "layout";
+	}
+	@RequestMapping(value="/update/{seq}", method=RequestMethod.GET)
+	public String update(@PathVariable("seq") String seq, Model model) {
+		
+		Long io_seq = Long.valueOf(seq);
+		IoListVO vo = iolistDao.findBySeq(io_seq);
+		model.addAttribute("IO", vo);
+		model.addAttribute("BODY", "IOLIST_INPUT");
+		
+		return "layout";
+	}
+	
+	@RequestMapping(value="/delete/{seq}", method=RequestMethod.GET)
+	public String delete(@PathVariable("seq") String seq, Model model) {
+		
+		Long io_seq = Long.valueOf(seq);
+		int ret = iolistDao.delete(io_seq);
+		
+		
+		return "redirect:/iolist";
 	}
 	
 }
--- SpMVC-iolist-01/src/main/java/com/callor/iolist/persistance/IoListDao.java
+++ SpMVC-iolist-01/src/main/java/com/callor/iolist/persistance/IoListDao.java
@@ -2,6 +2,7 @@ package com.callor.iolist.persistance;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Select;
 
 import com.callor.iolist.models.IoListVO;
@@ -9,15 +10,19 @@ import com.callor.iolist.models.IoListVO;
 public interface IoListDao {
 	
 	
-	@Select(" SELECT * FROM tbl_iolist ORDER BY io_date DESC, io_time DESC ")
+//	@Select(" SELECT * FROM tbl_iolist ORDER BY io_date DESC, io_time DESC ")
 	public List<IoListVO> selectAll();
 	
-	@Select(" SELECT * FROM tbl_iolist WHERE io_seq = #{io_seq} ")
+//	@Select(" SELECT * FROM tbl_iolist WHERE io_seq = #{io_seq} ")
 	public IoListVO findBySeq(Long seq);
 	
+	public int insertOrUpdate(IoListVO vo);
 	
 	public int insert(IoListVO vo);
 	public int update(IoListVO vo);
+	
+	@Delete(" DELETE FROM tbl_iolist WHERE io_seq = #{io_seq} " )
+	
 	public int delete(Long seq);
 	
 	
