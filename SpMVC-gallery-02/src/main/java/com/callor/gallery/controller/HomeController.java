ec0b287f8bec947a697f4784981622b0040799c0
n96js <n96js@naver.com>
2024-04-24 11:50:27 +0900

    thymeleaf

--- SpMVC-gallery-02/src/main/java/com/callor/gallery/controller/HomeController.java
+++ SpMVC-gallery-02/src/main/java/com/callor/gallery/controller/HomeController.java
@@ -1,6 +1,7 @@
-package com.callor.gallery;
+package com.callor.gallery.controller;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,25 +11,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.callor.gallery.dao.GalleryDao;
 import com.callor.gallery.models.GalleryVO;
 
+
 @Controller
-@RequestMapping(value="/gallery")
-public class GalleryController {
+public class HomeController {
 	
-	private GalleryDao galleryDao;
+	private final GalleryDao galleryDao;
 	
 	
-	public GalleryController(GalleryDao galleryDao) {
+	
+	public HomeController(GalleryDao galleryDao) {
 		super();
 		this.galleryDao = galleryDao;
 	}
 
 
-	@RequestMapping(value= {"","/"}, method=RequestMethod.GET)
-	public String home(Model model) {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
 		
+		model.addAttribute("NAME", "나는 이몽룡입니다");
 		List<GalleryVO> gList = galleryDao.selectAll();
-		model.addAttribute("GALLERYS",gList);
-				
+		model.addAttribute("GALLERYS", gList);
+		
 		return "gallery/list";
 	}
+	
 }
---
+++ SpMVC-gallery-02/src/main/webapp/static/js/main_nav.js
@@ -0,0 +1,74 @@
+// main_nav 각 tag 의 className 을 키로 하는 객체 선언
+/**
+ * JSON type 의 객체
+ *  형식 = {Key : value}
+ * console.log(NAV_INDEX.home) 결과는 ? {url : "/"}
+ * console.log(NAV_INDEX.home.url) 결과는 ? "/"
+ * console.log(NAV_INDEX["home"]) 결과는 ? {url : "/"}
+ */
+const NAV_INDEX = {
+  home: { url: "/" },
+  notice: { url: "/bbs/notice" },
+  free: { url: "/bbs/free" },
+};
+document.addEventListener("DOMContentLoaded", () => {
+  const mainNav = document.querySelector("nav.main");
+  const navItems = mainNav.querySelectorAll("li");
+
+  mainNav.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.tagName === "LI") {
+      const className = target.classList[0];
+      const url = NAV_INDEX[className].url;
+      // alert(url);
+      document.location.href = `${rootPath}${url}`;
+      // nav 가 클릭 되었을때 active 한 UI 를 보여주기 위한 설정
+      // 그런데 nav 를 클릭하면 화면이 refresh 되버리기 때문에 의미가 없음
+      // navItems.forEach((item) => {
+      //   // 혹시 li tag에 active 가 있으면 모두 지워
+      //   // 모든 nav.main li tag 에 active 라는 class 를 제거하기
+      //   item.classList.remove("active");
+      // });
+      // // 클릭된 항목에 active 라는 클래스를 추가하기
+      // target.classList.add("active");
+    }
+  });
+
+  // 화면이 새로 refresh 되었을때, 또는 화면이 최초로 보일때 실행할 코드
+  // 현재 화면이 보여지는 URL
+  // http://localhost:8080/gallery/bbs/free/write
+  const path = document.location.pathname;
+  // "/" 를 기준으로 하나씩 쪼갬
+  // pathname 을 슬래시("/")를 기준으로 단어단위로 자르기
+  const pathArr = path.split("/");
+
+  // localhost:8080/gallery/ 로 끝나면 localhost:8080/gallery/home 이라고 만들어라
+  pathArr[pathArr.length - 1] || pathArr.push("home");
+
+  // URL 중에서 가장 끝에 오는 URL 문자열은 ?
+  // 가장 끝에 문자열이 ""이면 "home" 문자열로 대체
+  // const pathname = pathArr[pathArr.length - 1] || "home";
+
+  // nav 가 1단계만 있을 경우
+  // document.querySelector(`nav.main li.${pathname}`)?.classList.add("active");
+
+  // nav 가 2단계만 있을 경우
+  // json 을 배열형태로 바꿔줌
+  // JSON type 의 데이터를 Array type 의 데이터로 변환하기
+  // const navs = Object.values(NAV_INDEX); NAV_INDEX의 밸류들만
+  // navs.forEach((nav) => {
+  // 분해한 url 중 nav.name 과 일치한 친구가 있으면
+  // 배열.join("sp") : 배열을 sp 문자열을 중간에 추가하여 하나의 문자열로 바꾸는 함수
+  // 분해된걸 다시 합쳐서 className 이 포함돼있으면
+  // if (pathArr.join(" ").includes(nav.name)) {
+  // document.querySelector(`nav.main li.${nav.name}`)?.classList.add("active");
+  // }
+  // });
+  // navs 는 home, notice, free 라는 요소가 있는 배열이 된다
+  const navs = Object.keys(NAV_INDEX); // NAV_INDEX 의 키들만
+  navs.forEach((key) => {
+    if (pathArr.join(" ").includes(key)) {
+      document.querySelector(`nav.main li${key}`)?.classList.add("active");
+    }
+  });
+});
