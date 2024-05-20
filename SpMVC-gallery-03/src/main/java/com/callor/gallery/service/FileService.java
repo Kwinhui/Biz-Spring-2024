677b6140fd301dc99d39ea58c45a4ff5533fb407
n96js <n96js@naver.com>
2024-05-20 14:40:10 +0900

    naverapi

--- SpMVC-gallery-03/src/main/java/com/callor/gallery/service/FileService.java
+++ SpMVC-gallery-03/src/main/java/com/callor/gallery/service/FileService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.callor.gallery.models.ImageVO;
+
 public interface FileService {
 	// single file upload
 	public String fileUp(MultipartFile file) throws Exception;
 	// multi file upload
-	public List<String> filesUp(MultipartHttpServletRequest files);
+	public List<ImageVO> filesUp(MultipartHttpServletRequest files) throws Exception;
 }
